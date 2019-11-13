4ffd1780afa458e80f7464d4de79f26eacb6a8f4
iceberg1109 <smartdev951109@gmail.com>
2019-11-13 12:23:31 +0800

    fix logout

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -60,6 +60,7 @@ export default class SplashScreen extends Component {
                             });
                         }
                     }}
+                    onLogoutFinished={() => console.log('logout.')}
                 /> */}
             </SafeAreaView>
         );
