0d1d8049ebf5db183eeaf3d84f68874309a62976
iceberg1109 <smartdev951109@gmail.com>
2019-11-14 12:32:51 +0800

    add styles to active list

--- src/components/ActiveList/styles.js
+++ src/components/ActiveList/styles.js
@@ -5,20 +5,14 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingLeft: 16,
-        paddingRight: 12,
-        paddingVertical: 8
     },
     userName: {
         flex: 1,
         textAlign: 'left',
-        paddingLeft: 8,
-        fontSize: 15
     },
     wave: {
         width: 28,
         height: 28,
-        resizeMode: 'contain'
     }
 });
 
